e0bc1a3ae0fc3f11ffa11793156df718fce78c79
Mayank Gupta <mayankgupta173@gmail.com>
2016-02-21 14:41:52 -0800

    add function to get current time for playing track

--- RNAudioPlayerURL.m
+++ RNAudioPlayerURL.m
@@ -16,12 +16,12 @@ RCT_EXPORT_METHOD(initWithURL:(NSString *)url){
   self.audioItem = [AVPlayerItem playerItemWithURL:soundUrl];
   self.audioPlayer = [AVPlayer playerWithPlayerItem:self.audioItem];
   [[NSNotificationCenter defaultCenter]
-  	addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.audioItem];
+    addObserver:self selector:@selector(playerItemDidReachEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.audioItem];
 }
 
 - (void)playerItemDidReachEnd:(NSNotification *)notification{
-	[self.audioItem seekToTime:kCMTimeZero];
-	[self.bridge.eventDispatcher sendAppEventWithName:@"AudioEnded" body:@{@"event": @"finished"}];
+  [self.audioItem seekToTime:kCMTimeZero];
+  [self.bridge.eventDispatcher sendAppEventWithName:@"AudioEnded" body:@{@"event": @"finished"}];
 }
 
 RCT_EXPORT_METHOD(getDuration:(RCTResponseSenderBlock)callback){
@@ -29,6 +29,13 @@ RCT_EXPORT_METHOD(getDuration:(RCTResponseSenderBlock)callback){
   }  //this is kind of crude but it will prevent the app from crashing due to a "NAN" return(this allows the getDuration method to be executed in the componentDidMount function of the React class without the app crashing
   float duration = CMTimeGetSeconds(self.audioItem.duration);
   callback(@[[[NSNumber alloc] initWithFloat:duration]]);
+}
+
+RCT_EXPORT_METHOD(getCurrentTime:(RCTResponseSenderBlock)callback){
+  while(self.audioItem.status != AVPlayerItemStatusReadyToPlay){
+  }
+  float currentTime = CMTimeGetSeconds(self.audioItem.currentTime);
+  callback(@[[[NSNumber alloc] initWithFloat:currentTime]]);
 }
 
 RCT_EXPORT_METHOD(play){
@@ -40,7 +47,8 @@ RCT_EXPORT_METHOD(pause){
 }
 
 RCT_EXPORT_METHOD(seekToTime:(nonnull NSNumber *)toTime){
-	[self.audioPlayer seekToTime: CMTimeMakeWithSeconds([toTime floatValue], NSEC_PER_SEC)];
+  [self.audioPlayer seekToTime: CMTimeMakeWithSeconds([toTime floatValue], NSEC_PER_SEC)];
 }
 
 @end
+
